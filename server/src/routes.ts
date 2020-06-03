ad4c7031ce44632f9c214dac7359679230317e42
Gustavo Castro <gustavocastrocs@gmail.com>
2020-06-03 10:47:17 -0300

    Criacao da API

---
+++ server/src/routes.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsControllers';
+import ItemsController from './controllers/ItemsControllers';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
+
+// Padroes da comunidade
+// index: para exibir uma listagem
+// show: para exibir um unico registro
+// cretate: 
+// update: 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,14 @@
 import express from 'express';
+import cors from 'cors'
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  //Request: utilizado para obter dados sobre a nossa requisicao.
-  //Response: devolver uma reposta para nosso browser ou apliacao
-  console.log('Listagem de users.');
-  response.json([
-    'Diego',
-    'Clayton',
-    'Robson',
-    'Gustavo'
-  ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
